9597a323bf07ee4a5d6e6681ff172eb9daada79e
Swapnil Singh <swapnilskumars@gmail.com>
2023-11-26 13:25:39 +0530

    setup user controller and user routes

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
 app.use(cookieParser())
 
+// routes import
+import userRoutes from "./routes/user.routes.js"
+
+// routes declare
+app.use("/user", userRoutes)
+
 export default app
